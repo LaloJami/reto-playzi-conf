fdd323291110ba5a4cef80dd855cd5eafe291663
LaloJami <eduardolalojami@gmail.com>
2021-10-28 12:36:25 -0500

    [update] flujo de trabajo con ESLint y prettier

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import '../styles/components/App.css'
+import React from 'react';
+
+import '../styles/components/App.css';
 
 const App = () => {
   return (
     <div>
       <h1>hola mundo</h1>
     </div>
-  )
-}
+  );
+};
 
 export default App;
