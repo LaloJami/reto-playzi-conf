819e923456ef536aac6517f37947ec50036fb9b3
LaloJami <eduardolalojami@gmail.com>
2021-10-28 12:04:34 -0500

    [updat] configuracion de loader y estilos

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 
 module.exports = {
   entry: "./src/index.js",
@@ -26,6 +28,13 @@ module.exports = {
             loader: "html-loader",
           },
         ],
+        test: /\.css$/,
+        use: [ 
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          'css-loader',
+        ],
       },
     ],
   },
@@ -34,10 +43,14 @@ module.exports = {
       template: "./public/index.html",
       filename: "./index.html",
     }),
+    new MiniCssExtractPlugin({
+      filename: 'assets/[name].css',
+    })
   ],
   devServer: {
-    contentBase: path.join(__dirname, "dist"),
+    static: path.join(__dirname, "dist"),
     compress: true,
     port: 3000,
+    open: true
   },
 };
