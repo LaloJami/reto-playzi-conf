9e8f3c2dd5587afde37f9e27ad020d93596c5864
LaloJami <eduardolalojami@gmail.com>
2021-10-29 13:42:29 -0500

    [update] implementacion de useContext

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -3,21 +3,24 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Layout from '../components/Layout';
 
 import {Home, Checkout, Information, Payment, Success, NotFound} from '../containers';
+import { AppProvider } from '../context/AppContext';
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Layout>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/checkout" component={Checkout} />
-          <Route exact path="/checkout/information" component={Information} />
-          <Route exact path="/checkout/payment" component={Payment} />
-          <Route exact path="/checkout/success" component={Success} />
-          <Route component={NotFound} />
-        </Switch>
-      </Layout>
-    </BrowserRouter>
+    <AppProvider>
+      <BrowserRouter>
+        <Layout>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/checkout" component={Checkout} />
+            <Route exact path="/checkout/information" component={Information} />
+            <Route exact path="/checkout/payment" component={Payment} />
+            <Route exact path="/checkout/success" component={Success} />
+            <Route component={NotFound} />
+          </Switch>
+        </Layout>
+      </BrowserRouter>
+    </AppProvider>
   );
 };
 
